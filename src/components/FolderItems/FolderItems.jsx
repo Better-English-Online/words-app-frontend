b96e2b1cad96eee9b4ca7c34cf44b4fa8ffc12ee
Rostislav Zhuravsky <rostislav.zhuravsky@gmail.com>
2022-03-30 18:36:01 +0300

    Enable automatic deploys to github pages
    * Added .github workflows
    * Fixed a build issue

--- src/components/FolderItems/FolderItems.jsx
+++ src/components/FolderItems/FolderItems.jsx
@@ -3,24 +3,25 @@ import FolderItem from '../FolderItem/index';
 import SetItem from '../SetItem/index';
 import styles from './index.module.css'
 
+const detectItem = (item) => {
+    if (item.type === 'folder') {
+        return (
+            <NavLink to={`/folders/${item.id}`}>
+                <FolderItem type={item.type} title={item.title}/>
+            </NavLink>
+        )
+    } else if(item.type ==='set') {
+    return (
+        <NavLink to={`/sets/${item.id}`}>
+            <SetItem  type={item.type} title={item.title}/>
+        </NavLink>
+    )}
+};
+
 const FolderItems = ({items}) => {
     return (
         <div className={styles.container}>
-        {items.map(item => {
-                if (item.type === 'folder') {
-                    return <NavLink
-                        to={`/folders/${item.id}`}
-                    >
-                        <FolderItem type={item.type} title={item.title}/>
-                    </NavLink>
-            } else if(item.type ==='set') {
-                return <NavLink
-                    to={`/sets/${item.id}`}
-                >
-                    <SetItem  type={item.type} title={item.title}/>
-                </NavLink>;
-            }
-        })}
+        {items.map(item => detectItem(item))}
         </div>
     )
 }
