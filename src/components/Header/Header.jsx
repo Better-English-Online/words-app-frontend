11eb0eb088cd55215a48636f3758fa949caad9bb
yaromir-zhuravsky <yaromir.zhuravsky@gmail.com>
2022-04-01 00:34:10 +0300

    Add more adaptivity for phones and tablets users

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import styles from './Header.module.css'
 import {NavLink} from 'react-router-dom'
+import HeaderButton from '../UI/HeaderButton/index';
 
 
 const Header = () => {
     return (
         <div className={styles.header}>
-            <NavLink to={"/"}>Home</NavLink>
-            <NavLink to={"/folders"}>Dictionary</NavLink>
+            <NavLink to={"/"}>
+                <HeaderButton text='Home'/>
+            </NavLink>    
+                <NavLink to={"/folders"}>
+                    <HeaderButton text='Dictionary'/>
+                </NavLink>
         </div>
     );
 }
---
+++ src/components/UI/HeaderButton/index.jsx
@@ -0,0 +1,10 @@
+import styles from './index.module.css'
+
+
+const HeaderButton = ({text}) => {
+    return (
+            <div className={styles["header-btn"]}>{text}</div>
+    );
+}
+
+export default HeaderButton
