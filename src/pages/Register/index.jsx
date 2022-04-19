98ae5d246fb6ec06757d2465c9575694c259342a
woarewe <rostislav.zhuravsky@gmail.com>
2022-04-20 01:11:59 +0300

    Add register page

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,11 @@
+import Page from "../../components/Page/index";
+
+const Register = () => {
+    return (
+        <Page>
+            <h1>Regiter</h1>
+        </Page>
+    );
+};
+
+export default Register;
