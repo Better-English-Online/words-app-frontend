8f803fa1c7d741774292d64692714b2b2a0f36f6
woarewe <rostislav.zhuravsky@gmail.com>
2022-04-20 01:44:28 +0300

    Check tailwind css

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -3,7 +3,14 @@ import Page from "../../components/Page/index";
 const Register = () => {
     return (
         <Page>
-            <h1>Regiter</h1>
+           <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                <div class="shrink-0">
+                </div>
+                <div>
+                    <div class="text-xl font-medium text-black">ChitChat</div>
+                    <p class="text-slate-500">You have a new message!</p>
+                </div>
+            </div>
         </Page>
     );
 };
