cb1679c345716ff2ea5b7778d631462134317278
yaromir-zhuravsky <yaromir.zhuravsky@gmail.com>
2022-04-20 14:59:28 +0300

    Add individual input for password

--- src/components/AuthForm/index.jsx
+++ src/components/AuthForm/index.jsx
@@ -1,38 +1,20 @@
 import styles from './index.module.css'
 import SubmitButton from '../UI/SubmitButton/index';
-import {useState} from 'react'
 import Input from "../../components/UI/Input/index";
+import PasswordInput from "../../components/UI/PasswordInput/index"
 
 
 const AuthForm = () => {
-    const [showPassword, setShowPassword] = useState(false)
-    const toggleShowPassword = () => {
-        setShowPassword(!showPassword)
-    }
-    const passwordInputType = () => {
-        return showPassword ? 'text' : 'password'
-    }
     return (
         <div className={styles.container}>
             <h2 className={styles["form-title"]}>Log-in</h2>
             <form className={styles.AuthForm} action="get">
-                <div style={{ width: "100%" }}>
+                <div className={styles.inputs} style={{ width: "100%" }}>
                     <Input type="text" placeholder="Mail..." name="mail" />
-                    <Input
-                        type={passwordInputType()}
-                        placeholder="Password..."
-                        name="password"
-                    />
+                    <PasswordInput/>
                 </div>
-                <input
-                    onClick={toggleShowPassword}
-                    checked={showPassword}
-                    type="checkbox"
-                    id="showPassword"
-                />
-                <label htmlFor="showPassword">show password</label>
                 <SubmitButton
-                    style={{ marginTop: "100px", width: "90%" }}
+                    style={{ marginTop: "100px", width: "100%" }}
                     text="Submit"
                 />
             </form>
---
+++ src/components/UI/PasswordInput/index.jsx
@@ -0,0 +1,75 @@
+import styles from "./index.module.css";
+import {useState} from 'react'
+
+const PasswordInput = () => {
+    const [showPassword, setShowPassword] = useState(false)
+
+    const showPasswordIcoStyle = {
+        position: 'absolute',
+        top: '50%',
+        transform: 'translateX(calc(-100% - 20px)) translateY(-50%)',
+        height: '22px',
+        cursor: 'pointer',
+        color: 'grey',
+        userSelect: 'none',
+    }
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+
+    const setPasswordInputType = () => {
+        return showPassword ? 'text' : 'password'
+    }
+
+    const setShowPasswordIcon =() => {
+        if (!showPassword) {
+            return <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                onClick={toggleShowPassword}
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                style={showPasswordIcoStyle}
+            >
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                <circle cx="12" cy="12" r="3"></circle>
+            </svg>
+        } else {
+            return (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-eye-off"
+                    style={showPasswordIcoStyle}
+                    onClick={toggleShowPassword}
+                >
+                    <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
+                    <line x1="1" y1="1" x2="23" y2="23"></line>
+                </svg>
+            );
+        }
+    }
+
+
+    return (
+        <div class={styles.container}>
+            <input
+                className={styles.passInput}
+                type={setPasswordInputType()}
+                placeholder='Password...'
+                name='password'
+            />
+            {setShowPasswordIcon()}
+        </div>
+    );
+};
+export default PasswordInput;
