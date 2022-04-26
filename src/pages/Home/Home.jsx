7794d17dc8d1513da47ed70ed9a6a2e4e0627fd5
yaromir-zhuravsky <yaromir.zhuravsky@gmail.com>
2022-04-26 22:53:38 +0300

    First attempts to parse data from api

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,31 @@
 import Page from "../../components/Page/index";
+import {useEffect, useState} from 'react'
 
 const Home = () => {
+    const [state, setState] = useState({
+        users: [],
+        isLoaded: false
+    })
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/')
+            .then(res => res.json())
+            .then(json => {
+                setState({users: json, isLoaded: true})
+            })
+    })
     return (
         <Page>
             <h1 style={{fontSize: '2.5rem'}}>Home</h1>
+                {(() => {
+                    if (!state.isLoaded) {
+                        return <h2>Loading...</h2>
+                    } else {
+                        return <ul>
+                            {state.users.map(user => <li key={user.id}>{user.name}</li>)}
+                        </ul>
+                    }
+                })()}
         </Page>
     );
 };
