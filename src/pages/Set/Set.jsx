d2a56120d4f5f2bbdc0440c8673276d3970828bf
yaromir-zhuravsky <yaromir.zhuravsky@gmail.com>
2022-04-30 18:44:57 +0300

    Try to deploy to deploy branch

--- src/pages/Set/Set.jsx
+++ src/pages/Set/Set.jsx
@@ -1,4 +1,4 @@
-import {useParams} from 'react-router-dom'
+// import {useParams} from 'react-router-dom'
 import Page from '../../components/Page';
 import NotFound from '../NotFound';
 
@@ -6,7 +6,7 @@ import NotFound from '../NotFound';
 
 
 const Set = () => {
-    const {id} = useParams()
+    // const {id} = useParams()
     const set = []
     if (!set) {
         return <NotFound />;
